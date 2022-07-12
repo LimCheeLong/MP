fd2a987bf66e00d1cc51aa26464e35ef703cb78f
LimCheeLong <92679502+LimCheeLong@users.noreply.github.com>
2022-07-12 23:50:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,7 @@ import threading
 import time
 import os
 
-customtkinter.set_appearance_mode("dark")
-#customtkinter.set_default_color_theme("green")  
+customtkinter.set_appearance_mode("dark")  
 
 root = customtkinter.CTk()
 root.title('Temperature Monitoring System App')
@@ -23,6 +22,16 @@ def Measure():
 def Tilt():
     print("tilting u dum bitch")
 
+def optionmenu(choice):
+    if choice == "Temperature Chart":
+        label.place_forget()
+        button1.place_forget()
+        button2.place_forget()
+    else:
+        label.place(relx=0.5, rely=0.01, anchor=tkinter.N)
+        button1.place(relx=0.32, rely=0.37, anchor=tkinter.CENTER)
+        button2.place(relx=0.68, rely=0.37, anchor=tkinter.CENTER)
+
 #Top Label
 label = customtkinter.CTkLabel(master=root,
                                textvariable=text_var,
@@ -33,6 +42,7 @@ label = customtkinter.CTkLabel(master=root,
                                text_font=("Roboto Medium","24"),
                                text_color=("black"))
 label.place(relx=0.5, rely=0.01, anchor=tkinter.N)
+
 
 #Measure Button
 button1 = customtkinter.CTkButton(master=root,
@@ -59,6 +69,19 @@ button2 = customtkinter.CTkButton(master=root,
                                  text_color=("black"),
                                  command=Tilt)
 button2.place(relx=0.68, rely=0.37, anchor=tkinter.CENTER)
+
+#OptionMenu for View between controls and Temperature
+optionmenu = customtkinter.CTkOptionMenu(master=root,
+                                       width=300,
+                                       height=50,
+                                       fg_color=("white", "light blue"),
+                                       text_font=("Roboto Medium","14"),
+                                       dropdown_text_font=("Roboto Medium","14"),
+                                       text_color=("black"),
+                                       values=["Controls", "Temperature Chart"],
+                                       command=optionmenu)
+optionmenu.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
+optionmenu.set("Controls")  # set initial value
 
 if __name__ == "__main__":
     root.mainloop()
